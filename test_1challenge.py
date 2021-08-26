73d86b0c370f7b848b9a21f57c7a60de1be7af8e
chmbrlnd <chmbrlnd@gmail.com>
2021-08-26 15:30:56 -0500

    Adding test unit.

---
+++ test_1challenge.py
@@ -0,0 +1,26 @@
+import unittest
+from os import path
+import numpy as np
+import pandas as pd
+
+class Test1Challenge(unittest.TestCase):
+
+	def test_file(self):
+		self.assertTrue(path.isfile('1challenge.csv'))
+
+	def test_columns(self):
+		submission_df = pd.read_csv('1challenge.csv', index_col=0)
+		self.assertIn('seq_length', submission_df.columns, 'Missing column seq_length')
+		self.assertTrue('sample_mean' in submission_df.columns, 'Missing column sample_mean')
+		self.assertTrue('sample_variance' in submission_df.columns, 'Missing column sample_variance')
+
+	def test_values(self):
+		submission_df = pd.read_csv('1challenge.csv', index_col=0)
+		sample_means = submission_df['sample_mean'].to_numpy()
+		sample_variances = submission_df['sample_variance'].to_numpy()
+		self.assertGreater(np.amin(np.absolute(sample_means)), 0, 'Mean values should be updated')
+		self.assertGreater(np.amin(np.absolute(sample_variances)), 0, 'Variance  values should be updated')
+
+if __name__ == '__main__':
+	unittest.main()
+
